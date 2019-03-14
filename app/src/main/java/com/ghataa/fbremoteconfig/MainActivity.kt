5836753462709154cb98fe1695ae5acdc586cba0
Gábor Horváth <dev.gaborhorvath@gmail.com>
2019-03-14 23:36:17 +0100

    Add Firebase Remote Config

--- app/src/main/java/com/ghataa/fbremoteconfig/MainActivity.kt
+++ app/src/main/java/com/ghataa/fbremoteconfig/MainActivity.kt
@@ -3,15 +3,58 @@ package com.ghataa.fbremoteconfig
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.ghataa.fbremoteconfig.databinding.ActivityMainBinding
+import com.ghataa.fbremoteconfig.util.Handlers
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var remoteConfig: FirebaseRemoteConfig
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
         binding.handlers = Handlers()
+
+        initFirebaseRemoteConfig()
+    }
+
+    fun initFirebaseRemoteConfig() {
+        remoteConfig = FirebaseRemoteConfig.getInstance()
+
+        val configSettings = FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                .build()
+
+        remoteConfig.setConfigSettings(configSettings)
+        remoteConfig.setDefaults(R.xml.remote_config_defaults)
+
+        fetchRemoteConfigParameters()
+    }
+
+    fun fetchRemoteConfigParameters() {
+        val cacheExpiration: Long = if (remoteConfig.info.configSettings.isDeveloperModeEnabled) {
+            0
+        } else {
+            3600
+        }
+
+        remoteConfig.fetch(cacheExpiration)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Fetch Succeeded",
+                                Toast.LENGTH_SHORT).show()
+
+                        // After config data is successfully fetched, it must be activated before newly fetched
+                        // values are returned.
+                        remoteConfig.activateFetched()
+                    } else {
+                        Toast.makeText(this, "Fetch Failed",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                }
     }
 }
