46e8c0221d28ed1e75e8e883eaa4000faeb9ce17
Gábor Horváth <dev.gaborhorvath@gmail.com>
2019-03-27 06:58:02 +0100

    Fetch asynchronous with RxJava2

--- app/src/main/java/com/ghataa/fbremoteconfig/SplashActivity.kt
+++ app/src/main/java/com/ghataa/fbremoteconfig/SplashActivity.kt
@@ -3,51 +3,39 @@ package com.ghataa.fbremoteconfig
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig
-import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+import com.ghataa.fbremoteconfig.util.RemoteConfigUtil
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class SplashActivity : AppCompatActivity() {
 
-    private lateinit var remoteConfig: FirebaseRemoteConfig
+    private var compositDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_splash)
 
-        initFirebaseRemoteConfig()
+        val initRemoteConfigDisposable = RemoteConfigUtil.init()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    navigateForward()
+                }, {
+                    navigateForward()
+                })
+
+        compositDisposable.add(initRemoteConfigDisposable)
     }
 
-    fun initFirebaseRemoteConfig() {
-        remoteConfig = FirebaseRemoteConfig.getInstance()
-
-        val configSettings = FirebaseRemoteConfigSettings.Builder()
-                .setDeveloperModeEnabled(BuildConfig.DEBUG)
-                .build()
-
-        remoteConfig.setConfigSettings(configSettings)
-        remoteConfig.setDefaults(R.xml.remote_config_defaults)
-
-        fetchRemoteConfigParameters()
+    override fun onPause() {
+        super.onPause()
+        compositDisposable.dispose()
     }
 
-    fun fetchRemoteConfigParameters() {
-        val cacheExpiration: Long = if (remoteConfig.info.configSettings.isDeveloperModeEnabled) 0 else 3600
-
-        remoteConfig.fetch(cacheExpiration)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        Toast.makeText(this, "Fetched", Toast.LENGTH_SHORT).show()
-
-                        remoteConfig.activateFetched()
-
-                        startActivity(Intent(this, MainActivity::class.java))
-                    } else {
-                        Toast.makeText(this, "Fetch Failed", Toast.LENGTH_SHORT).show()
-                    }
-
-                    finish()
-                }
+    fun navigateForward() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
--- app/src/main/java/com/ghataa/fbremoteconfig/util/RemoteConfigUtil.kt
+++ app/src/main/java/com/ghataa/fbremoteconfig/util/RemoteConfigUtil.kt
@@ -1,12 +1,57 @@
 package com.ghataa.fbremoteconfig.util
 
+import android.util.Log
+import com.ghataa.fbremoteconfig.BuildConfig
+import com.ghataa.fbremoteconfig.R
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+import io.reactivex.Completable
 
 class RemoteConfigUtil {
 
     companion object {
+
         const val MILLENNIUM_FALCON_TARGET_KEY = "millennium_falcon_target"
         const val MILLENNIUM_FALCON_TARGET_DEFAULT_VALUE = "tatooine"
+
+        @JvmStatic
+        fun init(): Completable {
+            val remoteConfig = FirebaseRemoteConfig.getInstance()
+
+            val configSettings = FirebaseRemoteConfigSettings.Builder()
+                    .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                    .build()
+
+            remoteConfig.setConfigSettings(configSettings)
+            remoteConfig.setDefaults(R.xml.remote_config_defaults)
+
+            return fetchRemoteConfigParameters(remoteConfig)
+        }
+
+        fun fetchRemoteConfigParameters(remoteConfig: FirebaseRemoteConfig): Completable {
+            return Completable.create { emitter ->
+
+                val cacheExpiration: Long = if (remoteConfig.info.configSettings.isDeveloperModeEnabled) 0 else 3600
+
+                remoteConfig.fetch(cacheExpiration)
+                        .addOnCompleteListener { task ->
+                            run {
+                                if (task.isSuccessful) {
+                                    Log.d("RemoteConfigUtil", "Fetched")
+
+                                    remoteConfig.activateFetched()
+                                    Log.d("RemoteConfigUtil", "Activated")
+
+                                    emitter.onComplete()
+                                } else {
+                                    Log.d("RemoteConfigUtil", "Fetch Failed")
+
+                                    emitter.onError(task.exception!!)
+                                }
+                            }
+                        }
+            }
+        }
 
         @JvmStatic
         fun targetIsTatooine(): Boolean {
